c597faededb23bdd11c5bbbe780cc1123b23bb7d
khokhlovap <polina_2304@bk.ru>
2024-11-27 23:38:16 +0300

    фильтр DjangoFilterBackend (фльтрация по кол-во комнат)

--- buildingsite/realty/views/view_sets_apartment.py
+++ buildingsite/realty/views/view_sets_apartment.py
@@ -1,14 +1,27 @@
+from django_filters.rest_framework import DjangoFilterBackend
 from django.shortcuts import get_list_or_404
 from rest_framework import viewsets, status
 from rest_framework.response import Response
 from realty.models import Apartment
 from django.db.models import Q
 from realty.serializers.apartment import ApartmentSerializer
+from django_filters import rest_framework as filters
 
-class ApartmentViewSet(viewsets.ViewSet):
+class ApartmentFilter(filters.FilterSet):
+    number_rooms = filters.NumberFilter()
+
+    class Meta:
+        model = Apartment
+        fields = ['number_rooms']  # Добавьте другие поля, если нужно
+
+class ApartmentViewSet(viewsets.ModelViewSet):
     serializer_class = ApartmentSerializer
+    queryset = Apartment.objects.all()
+    filter_backends = (DjangoFilterBackend,)
+    filterset_class = ApartmentFilter
+
     def list(self, request):
-        queryset = Apartment.objects.all()
+        queryset = self.filter_queryset(self.queryset)
 
         filter_condition = (
             (Q(square__lte=50) | Q(number_rooms__lte=2)) & 
@@ -16,5 +29,6 @@ class ApartmentViewSet(viewsets.ViewSet):
         )
         apartments = queryset.filter(filter_condition)
 
+        
         serializer = ApartmentSerializer(apartments, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
