81ed9fe63e24a330234c2c42c24719705dc754de
khokhlovap <polina_2304@bk.ru>
2024-12-06 00:36:31 +0300

    создан Q запрос для заявок с сайта

--- buildingsite/realty/views/models_view_set_applicationwebsite.py
+++ buildingsite/realty/views/models_view_set_applicationwebsite.py
@@ -4,11 +4,29 @@ from realty.models import ApplicationWebsite
 from realty.serializers.applicationwebsite import ApplicationWebsiteSerializer
 from rest_framework.decorators import action
 from realty.paginations.applicationwebsite import ApplicationPagination
+from django.db.models import Q
 
 class ApplicationModelViewSet(viewsets.ModelViewSet):
     queryset = ApplicationWebsite.objects.all()
     serializer_class = ApplicationWebsiteSerializer
     pagination_class = ApplicationPagination
+
+    @action(methods=['get'], detail=False)
+    def application_website_filter(self, request):
+    
+        """
+        Фильтр отражает заявки, которые одновременно имеют статус "Принято" и соответствуют хотя бы одному из условий 
+        (либо имя "Мария", либо номер телефона не содержит "2345")
+        """
+
+        filter_condition = (
+            Q(status_application='accepted') & 
+            (Q(name_client='Мария') | ~Q(number_phone__contains='3471'))
+        )
+        
+        application_filter = ApplicationWebsite.objects.filter(filter_condition)
+        serializer = ApplicationWebsiteSerializer(application_filter, many=True)
+        return Response(serializer.data)
 
     @action(methods=['post'], detail=True)
     def update_status(self, request, pk=None):
