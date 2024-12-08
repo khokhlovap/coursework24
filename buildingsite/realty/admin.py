1bda628ef721fc342d3b63c950f88a285bf4faab
khokhlovap <polina_2304@bk.ru>
2024-12-08 19:44:17 +0300

    экспорт в эксель с фильтрацией

--- buildingsite/realty/admin.py
+++ buildingsite/realty/admin.py
@@ -2,11 +2,11 @@ from django.contrib import admin
 from django.utils import timezone
 from .models import InfoBuilding, Apartment, StatusApartment, RegularCustomers, Deal2, ApplicationWebsite, ApartmentPhoto
 from import_export.admin import ImportExportModelAdmin, ExportActionMixin
-from realty.export import ApartmentResource
 from datetime import datetime
 from simple_history.admin import SimpleHistoryAdmin
+from import_export import resources
 
-class ApartmentInline(admin.TabularInline):  # или admin.StackedInline
+class ApartmentInline(admin.TabularInline): 
     model = Apartment
     extra = 1  # Количество пустых форм для добавления новых объектов
     
@@ -24,7 +24,20 @@ class ApartmentPhotoInline(admin.TabularInline):
     model = ApartmentPhoto
     extra = 1  # Количество пустых форм для добавления новых объектов
 
-class ApartmentAdmin(SimpleHistoryAdmin, ImportExportModelAdmin, ExportActionMixin, admin.ModelAdmin):
+class ApartmentResource(resources.ModelResource):
+    class Meta:
+        model = Apartment
+        fields = ('number_rooms', 'number_floor', 'square', 'price', 'code_building', 'apartment_code')
+
+    def dehydrate_square(self, obj):
+        """Форматируем площадь с указанием единиц измерения - кв.м """
+        return f'{obj.square} кв.м' 
+    
+    def dehydrate_price(self, apartment):
+        """Форматируем цену с указанием валюты - рубли"""
+        return f'{apartment.price:.2f} руб.'
+
+class ApartmentAdmin(SimpleHistoryAdmin, ImportExportModelAdmin,  ExportActionMixin, admin.ModelAdmin):
     resource_class = ApartmentResource
     list_display = ('number_rooms', 'number_floor', 'square', 'price', 'code_building', 'building_info', 'apartment_code')
     list_filter = ('number_rooms', 'number_floor', 'code_building',)
@@ -34,9 +47,15 @@ class ApartmentAdmin(SimpleHistoryAdmin, ImportExportModelAdmin, ExportActionMix
     inlines = [ApartmentPhotoInline]  # Добавляем inline
 
     def get_export_filename(self, *args, **kwargs):
-        # Получаем текущую дату для добавления в имя файла
-        current_date = datetime.now().strftime('%d.%m.%Y')
+        current_date = datetime.now().strftime('%d.%m.%Y') # Получаем текущую дату для добавления в имя файла
         return f'Apartments_export_{current_date}.csv'
+    
+    def get_export_queryset(self, request):
+        """Экспортируем только записи, где количество комнат >= 2"""
+        queryset = super().get_export_queryset(request)
+        print(request)
+        return queryset.filter(number_rooms__gte=2)
+    
 
 class ApartmentPhotoAdmin(SimpleHistoryAdmin, admin.ModelAdmin):
      list_display = ('apartment', 'image', 'description')
@@ -69,7 +88,24 @@ class Deal2Admin(SimpleHistoryAdmin, admin.ModelAdmin):
     def formatted_date_change(self, obj):
         return obj.data_deal.strftime('%d.%m.%Y')
 
-class ApplicationWebsiteAdmin(SimpleHistoryAdmin, admin.ModelAdmin):
+class ApplicationWebsiteResource(resources.ModelResource):
+    class Meta:
+        model = ApplicationWebsite
+        fields = ('name_client', 'number_phone', 'status_application', 'date_create3')
+
+    def dehydrate_status_application(self, application_website):
+        """Изменяем статус при экспорте"""
+        if application_website.status_application == 'accepted':
+            return "Принято"
+        else:
+            return "Отклонено"
+        
+    def dehydrate_date_create3(self, application_website):
+        # Изменяем формат даты на дд.мм.гг
+        return application_website.date_create3.strftime('%d.%m.%y')
+
+class ApplicationWebsiteAdmin(SimpleHistoryAdmin, ImportExportModelAdmin, ExportActionMixin, admin.ModelAdmin):
+    resource_class = ApplicationWebsiteResource
     list_display = ('formatted_date_change', 'name_client', 'number_phone', 'status_application')
     date_hierarchy = 'date_create3'
     readonly_fields = ('date_create3',)
@@ -78,6 +114,10 @@ class ApplicationWebsiteAdmin(SimpleHistoryAdmin, admin.ModelAdmin):
     @admin.display(description='Дата создания заявки')
     def formatted_date_change(self, obj):
         return obj.date_create3.strftime('%d.%m.%Y')
+    
+    def get_export_filename(self, *args, **kwargs):
+        current_date = datetime.now().strftime('%d.%m.%Y') # Получаем текущую дату для добавления в имя файла
+        return f'ApplicationWebsite_export_{current_date}.csv'
 
 admin.site.register(InfoBuilding, InfoBuildingAdmin)
 admin.site.register(Apartment, ApartmentAdmin)
