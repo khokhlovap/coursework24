809d644e874850843909080acf2bc9329a1fa187
khokhlovap <polina_2304@bk.ru>
2024-12-22 22:14:26 +0300

    подключение linter и swagger

--- buildingsite/buildingsite/settings.py
+++ buildingsite/buildingsite/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'import_export',
     'simple_history',
+    'drf_yasg',
 ]
 
 MIDDLEWARE = [
--- buildingsite/buildingsite/urls.py
+++ buildingsite/buildingsite/urls.py
@@ -18,12 +18,31 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+# Настройка схемы Swagger
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Building Site API",
+        default_version='v1',
+        description="API документация для Building Site",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@buildingsite.local"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path("realty/", include("realty.urls")),
     path("admin/", admin.site.urls),
     path('api/', include('realty.urls')),
-
+     path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
 
 if settings.DEBUG:
